6ca851a3672fd74ee50fb26bfbb2d8461e4abcda
yangjiakai-isid <xyang.jiakai@contract.isid.co.jp>
2021-10-18 09:51:29 +0900

    更新css

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,64 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="primary-bg">This is an about page</h1>
+    <p>bgbgbgb</p>
   </div>
 </template>
+<style lang="css">
+.about {
+  --primary-color: #7f583f;
+}
+
+.elevation-1 {
+  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.16), 0px 1px 2px rgba(0, 0, 0, 0.12),
+    0px 1px 2px rgba(0, 0, 0, 0.24);
+  border-radius: 2px;
+}
+
+.elevation-2 {
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.16), 0px 2px 4px rgba(0, 0, 0, 0.12),
+    0px 1px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 2px;
+}
+
+.elevation-3 {
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.02),
+    0px 1px 12px rgba(0, 0, 0, 0.12);
+  border-radius: 4px;
+}
+.elevation-4 {
+  box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.12), 0px 8px 16px rgba(0, 0, 0, 0.12),
+    0px 1px 24px rgba(0, 0, 0, 0.12);
+  border-radius: 4px;
+}
+.elevation-5 {
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.12), 0px 6px 12px rgba(0, 0, 0, 0.12),
+    0px 1px 16px rgba(0, 0, 0, 0.12);
+  border-radius: 4px;
+}
+.elevation-6 {
+  box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.12), 0px 8px 16px rgba(0, 0, 0, 0.12),
+    0px 1px 24px rgba(0, 0, 0, 0.12);
+  border-radius: 4px;
+}
+.elevation-7 {
+  box-shadow: 0px 0px 18px rgba(0, 0, 0, 0.16), 0px 0px 28px rgba(0, 0, 0, 0.16),
+    0px 0px 52px rgba(0, 0, 0, 0.16);
+  border-radius: 8px;
+}
+.elevation-8 {
+  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.12),
+    0px 12px 24px rgba(0, 0, 0, 0.12), 0px 1px 32px rgba(0, 0, 0, 0.12);
+  border-radius: 8px;
+}
+.elevation-9 {
+  box-shadow: 0px 12px 24px rgba(0, 0, 0, 0.12),
+    0px 16px 32px rgba(0, 0, 0, 0.12), 0px 1px 48px rgba(0, 0, 0, 0.12);
+  border-radius: 8px;
+}
+
+.card {
+  width: 500px;
+  height: 500px;
+}
+</style>
